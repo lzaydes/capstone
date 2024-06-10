406db1b008b19dee46f9a79a4b96968fdcd304c8
lzaydes <leozaydes@gmail.com>
2024-06-10 13:40:18 -0700

    Final code uploaded for submission

---
+++ src/OLED_RGB/capstone.sdk/sketch/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../sketch_bsp/microblaze_0/include/xparameters.h \
+ ../../sketch_bsp/microblaze_0/include/xil_cache.h \
+ ../../sketch_bsp/microblaze_0/include/mb_interface.h \
+ ../../sketch_bsp/microblaze_0/include/xil_types.h \
+ ../../sketch_bsp/microblaze_0/include/xil_assert.h \
+ ../../sketch_bsp/microblaze_0/include/xil_exception.h \
+ ../../sketch_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../sketch_bsp/microblaze_0/include/xparameters.h:
+
+../../sketch_bsp/microblaze_0/include/xil_cache.h:
+
+../../sketch_bsp/microblaze_0/include/mb_interface.h:
+
+../../sketch_bsp/microblaze_0/include/xil_types.h:
+
+../../sketch_bsp/microblaze_0/include/xil_assert.h:
+
+../../sketch_bsp/microblaze_0/include/xil_exception.h:
+
+../../sketch_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
